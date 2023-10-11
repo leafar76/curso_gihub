6265c608a03bd5aecfcf2901f4b8e346478322c2
Rafael Souza (OBJECT EDGE INC) <rafael.souza@motorolasolutions.com>
2023-10-11 16:49:41 -0300

    adicionando funcionalidade de mostrar msg

--- cadastro.js
+++ cadastro.js
@@ -1,5 +1,9 @@
 function signUp(email){
     return email;
 }
-
 //pegando o email e cadastrando na plataforma
+function mostraMensagem(){
+    return 'bem vido ao sisgtema';
+}
+
+//adicionando msg
